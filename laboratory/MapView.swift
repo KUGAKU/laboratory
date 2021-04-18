13a12579063f874fb63b946aadaa24da5905295a
gaku yamamoto <githubgemini1124@gmail.com>
2021-04-18 15:05:27 +0900

    implement map view

---
+++ laboratory/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  laboratory
+//
+//  Created by 山本学 on 2021/04/18.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 35.6221913, longitude: 139.7048446),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
