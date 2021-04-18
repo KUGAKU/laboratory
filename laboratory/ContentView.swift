348a43ec5185d5ebbcda5676606d3238fa3226a5
gaku yamamoto <githubgemini1124@gmail.com>
2021-04-18 10:41:32 +0900

    initial commit

---
+++ laboratory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  laboratory
+//
+//  Created by 山本学 on 2021/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("SwiftUI laboratory")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ laboratory/laboratoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  laboratoryApp.swift
+//  laboratory
+//
+//  Created by 山本学 on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct laboratoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
